b5c3a11a8a913760c6bc87574adfb4a365f691c5
amapuyan <anthonymapuyan06@gmail.com>
2025-09-06 02:14:48 -0400

    First steps

---
+++ scripts/loinc_processor.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+loinc = pd.read_csv("C:\Users\antho\OneDrive\Desktop\Masters\FALL 25\HHA 507\Homework\medical-codex-pipeline\input\LoincTable\Loinc.csv", dtype=str)
+
+loinc.info
