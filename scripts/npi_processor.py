fa5eb20082d8fb52ca975275eb183322531fcc05
Anthony Mapuyan <amapuyan@MacBook-Pro-5.local>
2025-09-10 15:48:59 -0400

    npi done

--- scripts/npi_processor.py
+++ scripts/npi_processor.py
@@ -22,20 +22,25 @@ print(f"\nDataset shape: {df_polars.shape}")
 print(f"\nFirst 5 rows:")
 print(df_polars.head())
 
-print(f"\nMemory usage (MB): {df.estimated_size() / 1024**2:.2f}")
+print(f"\nMemory usage (MB): {df_polars.estimated_size() / 1024**2:.2f}")
 
 df_polars_small = df_polars.select([
     'NPI',
     'Provider Last Name (Legal Name)'
 ])
 
+df_polars_small = df_polars_small.rename({
+    'NPI': 'code',
+    'Provider Last Name (Legal Name)': 'description',
+})
+
 df_polars_small = df_polars_small.with_columns(
     pl.lit('09-06-2025').alias('last_updated')
 )
 
-output_path = 'MEDICAL-CODEX-PIPELINE/output/npi_small.csv'
-df_polars_small.write_csv(output_path)
-df_polars_small.write_parquet('MEDICAL-CODEX-PIPELINE/output/npi_small.parquet')
+file_output_path = 'output/npi_small.csv'
+df_polars_small.write_csv(file_output_path)
+df_polars_small.write_parquet('output/npi_small.parquet')
 
 
 
